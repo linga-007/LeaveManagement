88f11fbc7cc0c299e6b2e26732467be4f0db9745
Mohammed Ashif A <123965886+ashif-2005@users.noreply.github.com>
2024-08-30 15:29:47 +0530

    Delete backend directory

--- backend/routes/mailRoute.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const {Email} = require('../controllers/mailController');
-const checkUser = require('../middleware/auth')
-
-router.post('/send',checkUser ,Email)
-
-module.exports = router;
--- backend/routes/tableRoute.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const {getDetails} = require('../controllers/tableController');
-const checkUser = require('../middleware/auth')
-
-router.post('/getDetails',checkUser ,getDetails)
-
-module.exports = router;
