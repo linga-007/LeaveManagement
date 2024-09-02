606a53d3f6414131cba1092e5938d2cc20c38d4b
Kishore-kumar-07 <bkdon54@gmail.com>
2024-09-03 00:07:43 +0530

    updated the ui and commited

--- frontend/src/pages/Employee/Components/UserDetailsCard.jsx
+++ frontend/src/pages/Employee/Components/UserDetailsCard.jsx
@@ -1,71 +1,91 @@
-import React from 'react'
-import UserImg from "../assets/user.png"
-import axios from 'axios'
-import { useEffect , useState } from 'react'
-import { jwtDecode } from 'jwt-decode';
-import GVRLOGO from "../../../images/GVR.png"
+import React, { useEffect, useState } from 'react';
+import GVR from '../../../images/GVRLogo.png';
+import LogOut from '../../../images/logout.png';
+import user from '../../../images/profile.png';
+import axios from 'axios';
+import {jwtDecode} from 'jwt-decode';
 
+function Nav() {
+  const [response, setResponse] = useState({});
+  const [showDetails, setShowDetails] = useState(false);
 
+  const token = document.cookie.split('=')[1];
+  const decodedToken = jwtDecode(token);
+  const empId = decodedToken.empId;
 
- 
-function UserDetailsCard() {
-
-  const [response,setResponse] = useState({});
- 
-  const token = document.cookie.split('=')[1]
-  const decodedToken = jwtDecode(token)
-  const empId = decodedToken.empId
-
-
-  console.log("token ",empId)
-
-  useEffect(()=>{
- 
+  useEffect(() => {
     getData();
-  },[])
+  }, []);
 
-  const getData = async() =>{
+  const getData = async () => {
     try {
-      const res = await axios.post("http://localhost:5000/emp/getEmp",{
-      empId
-      },
-       {
-
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      });
+      const res = await axios.post(
+        'http://localhost:5000/emp/getEmp',
+        { empId },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
       setResponse(res.data[0]);
-      console.log(res);
-      console.log( )
-   
-  
     } catch (error) {
-      console.error("Error fetching user details:", error);
+      console.error('Error fetching user details:', error);
     }
-  }
+  };
 
-    var title = ["Desigination: ","Repoting Manager: ","DOJ: ","Function: ","Department: ","Band/Level: ","Location: "];
-    var details = [response.designation , response.reportionManager,  response.dateOfJoining , response.function , response.department , response.level , response.location    ];
+  const title = [
+    'Designation: ',
+    'Reporting Manager: ',
+    'DOJ: ',
+    'Function: ',
+    'Department: ',
+    'Band/Level: ',
+    'Location: ',
+  ];
+  const details = [
+    response.designation,
+    response.reportionManager,
+    response.dateOfJoining,
+    response.function,
+    response.department,
+    response.level,
+    response.location,
+  ];
+
   return (
-    <div className='w-[25%]  bg-[#000046]  '>
-      <div className=''>
-          <img src={GVRLOGO}/> 
+    <>
+      <div className='w-full flex justify-center items-center pl-6 pr-6 rounded-lg'>
+        <div className='w-[100%] shadow-md flex justify-between items-center px-5 py-2 rounded-lg'>
+          <div className='text-xl font-semibold'>
+            <img src={GVR} className='h-10' />
           </div>
- <div className='w-[100%] text-white flex flex-col justify-center items-center '>
-    <img src={UserImg} className='h-[10vh]'/>
-    <div className='flex flex-col justify-center items-center'>
-       <h1 className='text-xl font-bold'>{response.empName}</h1>
-       <div>
-       {title.map((val,index)=><h1> <span className='text-pink-700 text-lg font-semibold'>{val}</span> {details[index]}</h1>)}
-
-       </div>
-    </div>
-    </div>
-    </div>
-   
-  )
+          <div>
+            <button
+              className='flex justify-center items-center'
+              onClick={() => setShowDetails(!showDetails)}
+            >
+              <img src={user} className='h-10' />
+            </button>
+            {showDetails && (
+              <div className='absolute mt-2 bg-white shadow-md rounded-lg p-4 text-black'>
+                <h1 className='text-xl font-bold'>{response.empName}</h1>
+                <div className='text-sm'>
+                  {title.map((val, index) => (
+                    <h1 key={index}>
+                      <span className='text-gray-700 font-semibold'>{val}</span>{' '}
+                      {details[index]}
+                    </h1>
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default UserDetailsCard
+export default Nav;
